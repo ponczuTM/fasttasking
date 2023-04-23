960481426d1b30c3a6c20e92f4f9f6c81b5e3742
KAR0LLL <jozefkowicz.karol@gmail.com>
2023-04-23 15:58:26 +0200

    wstępne logowanie

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages";
+import Login from "./pages/Login";
+import Logout from "./pages/Logout";
 import React from "react";
 import User from "./pages/User";
 import Role from "./pages/Role";
@@ -12,6 +14,8 @@ function App() {
 				<Route exact path="/" element={<Home/>} />
 				<Route exact path="/user" element={<User/>} />
 				<Route exact path="/role" element={<Role/>} />
+				<Route exact path="/login" element={<Login/>} />
+				<Route exact path="/logout" element={<Logout/>} />
 			</Routes>
 		</Router>
 	);
